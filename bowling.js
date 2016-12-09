18f1230af7f9772a9482d05c04471b0c374fad64
Ophir <ophir@Ophir-MacBook-Pro.local>
2016-12-09 22:49:27 +0100

    Solution groupe 2

--- bowling.js
+++ bowling.js
@@ -1,19 +1,71 @@
 class Bowling {
-    works() {
-        return true;
-    }
-}
 
-/*
-function Bowling() {
-    function works() {
-        return true;
+    constructor() {
+        this.frames = [];
+        this.currentFrame = [];
     }
 
-    return {
-        works
+    roll(pins) {
+        this.currentFrame.push(pins);
+        if(this.frameIsFinished()) {
+            this.createNewFrame();
+        }
+    }
+
+    createNewFrame() {
+        this.frames.push(this.currentFrame);
+        this.currentFrame = [];
+    }
+
+    score() {
+        return this.frames
+            .map((frame) =>
+                this.framePoints(frame))
+            .reduce((sum, points) =>
+                sum + points, 0);
+    }
+
+    framePoints(frame) {
+        return this.framePins(frame) + this.frameBonus(frame);
+    }
+
+    frameBonus(frame) {
+        if (this.isStrike(frame)) {
+            return this.strikeBonus(frame);
+        } else if (this.isSpare(frame)) {
+            return this.spareBonus(frame);
+        } else {
+            return 0;
+        }
+    }
+
+    spareBonus(frame) {
+        let frameIndex = this.frames.indexOf(frame);
+        return this.frames[frameIndex + 1][0];
+    }
+
+    strikeBonus(frame) {
+        let frameIndex = this.frames.indexOf(frame);
+        return this.frames[frameIndex + 1][0] + this.frames[frameIndex + 1][1];
+    }
+
+    isSpare(frame) {
+        return this.framePins(frame) == 10;
+    }
+
+    isStrike(frame) {
+        return frame[0] == 10;
+    }
+
+    framePins(frame) {
+        return frame.reduce((sum, pins) =>
+            sum + pins, 0);
+    }
+
+    frameIsFinished() {
+        return (this.currentFrame.length == 2)
+            || this.isStrike(this.currentFrame);
     }
 }
-*/
 
 module.exports = Bowling;
--- bowling.spec.js
+++ bowling.spec.js
@@ -1,10 +1,66 @@
-let Bowling = require('./bowling')
+let Bowling = require('./bowling');
 
-describe("bowling", () => {
+describe("bowling", () =>{
+    let bowling;
 
-    it("works", () => {
-        let bowling = new Bowling();
-        expect(bowling.works()).toBe(true);
+    beforeEach(() =>{
+        bowling = new Bowling();
+    });
+
+    function repeat(n, f) {
+        for (let i = 0; i <n; i++) {
+            f();
+        }
+    }
+
+    function roll(pins) {
+        bowling.roll(pins);
+    }
+
+    function rollZero() {
+        roll(0);
+    }
+
+    function rollOne() {
+        roll(1);
+    }
+
+    function rollSpare() {
+        roll(5);
+        roll(5);
+
+    }
+
+    function rollStrike() {
+        roll(10);
+    }
+
+    function expectScore(score) {
+        expect(bowling.score()).toEqual(score);
+    }
+
+    it('returns 0 for a gutter game', () =>{
+        repeat(20, rollZero);
+        expectScore(0);
+    });
+
+    it('returns 0 if only 1 are rolled', () =>{
+        repeat(20, rollOne);
+        expectScore(20);
+    });
+
+    it('handles a spare', function () {
+        rollSpare();
+        roll(3);
+        repeat(17, rollZero);
+        expectScore(16);
+    });
+    it('handles a strike', function () {
+        rollStrike();
+        roll(3);
+        roll(2);
+        repeat(16, rollZero);
+        expectScore(20);
     });
 
 });
