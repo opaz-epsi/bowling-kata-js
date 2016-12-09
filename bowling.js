d396781e1dd032be6e30f23f76f5133fdc50319f
Ophir <ophir@Ophir-MacBook-Pro.local>
2016-12-09 16:02:53 +0100

    Session groupe 1

--- bowling.js
+++ bowling.js
@@ -1,19 +1,42 @@
 class Bowling {
-    works() {
-        return true;
+
+    constructor() {
+        this.rolls = []
+    }
+
+    roll(pins) {
+        this.rolls.push(pins);
+    }
+
+    score() {
+        let sum = 0;
+        for(let i = 0; i < this.rolls.length; i++) {
+            if(this.isSpare(i)) {
+                sum += this.spareBonus(i);
+            }
+            sum += this.rolls[i];
+        }
+        return sum;
+    }
+
+    spareBonus(i) {
+        return this.rolls[i + 1];
+    }
+
+    isSpare(rollIndex) {
+        return this.frameFinished(rollIndex)
+        && this.pinsCount(rollIndex) == 10;
+
+    }
+
+    frameFinished(rollIndex) {
+        return rollIndex % 2 != 0;
+    }
+
+    pinsCount(rollIndex) {
+        return this.rolls[rollIndex] + this.rolls[rollIndex - 1];
     }
 }
 
-/*
-function Bowling() {
-    function works() {
-        return true;
-    }
-
-    return {
-        works
-    }
-}
-*/
 
 module.exports = Bowling;
--- bowling.spec.js
+++ bowling.spec.js
@@ -1,10 +1,49 @@
 let Bowling = require('./bowling')
 
 describe("bowling", () => {
+    let bowling;
 
-    it("works", () => {
-        let bowling = new Bowling();
-        expect(bowling.works()).toBe(true);
+    function repeat(n, f) {
+        for (let i = 0; i < n; i++) {
+            f()
+        }
+    }
+
+    function roll(pins) {
+        bowling.roll(pins);
+    }
+
+    function rollZero() {
+        roll(0);
+    }
+
+    function rollOne() {
+        roll(1);
+    }
+
+    function expectScore(score) {
+        expect(bowling.score()).toEqual(score);
+    }
+
+    beforeEach(() =>{
+        bowling = new Bowling()
+    });
+
+    it('returns 0 for a gutter game', () =>{
+        repeat(20, rollZero);
+        expectScore(0);
+    });
+
+    it('returns 20 if only 1 are rolled', () =>{
+        repeat(20, rollOne);
+        expectScore(20);
+    });
+    it('handles a spare', () =>{
+        roll(5);
+        roll(5);
+        roll(2);
+        repeat(17, rollZero);
+        expectScore(14);
     });
 
 });
